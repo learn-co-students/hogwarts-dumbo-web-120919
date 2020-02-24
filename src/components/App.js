0a200aa9a4bba67448461a2aae8e21e84f874696
Gracie <m.graciemcguire@gmail.com>
2020-02-24 12:45:13 -0500

    adds review

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,40 @@ import React, { Component } from "react";
 import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+import PigPen from "./PigPen";
 
 class App extends Component {
+
+  state = {
+    hogs: hogs,
+    showGreased: false,
+    sortByName: false,
+    sortByWeight: false
+  }
+
+  // these are some functions to sort the piggies
+  handleGreasedBtnClick = () => {
+    this.setState({
+      showGreased: !this.state.showGreased
+    })
+  }
+  handleSortNameClick = () => {
+
+  }
+  handleSortWeightClick = () => {
+
+  }
+  // end sorting
+
   render() {
     return (
       <div className="App">
-        <Nav />
-        <HelloWorld />
+        <Nav
+          handleGreasedBtnClick={this.handleGreasedBtnClick}
+          handleSortNameClick={this.handleSortNameClick}
+          handleSortWeightClick={this.handleSortWeightClick}
+        />
+      <PigPen hogs={this.state.showGreased ? this.state.hogs.filter( hog => hog.greased ) : this.state.hogs} />
       </div>
     );
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,11 @@
 import piggy from "../porco.png";
 import React from "react";
 
-const Nav = () => {
+import FilterGreased from './FilterGreased'
+import SortName from './SortName'
+import SortWeight from './SortWeight'
+
+const Nav = (props) => {
   return (
     <div className="navWrapper">
       <span className="headerText">Hogwarts</span>
@@ -9,6 +13,13 @@ const Nav = () => {
         <img src={piggy} className="App-logo" alt="piggy" />
       </div>
       <span className="normalText">A React App for County Fair Hog Fans</span>
+      <div className="themButtons">
+        <FilterGreased
+          handleGreasedBtnClick={props.handleGreasedBtnClick}
+        />
+        <SortName />
+        <SortWeight />
+      </div>
     </div>
   );
 };
