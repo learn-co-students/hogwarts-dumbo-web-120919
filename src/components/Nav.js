9a13bfabe4dadbe4cbc2677a5f3f69cbd8448a78
David Cha <davidcha17@yahoo.com>
2020-03-12 19:18:38 -0400

    Done.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import piggy from "../porco.png";
 import React from "react";
 
-const Nav = () => {
+const Nav = (props) => {
   return (
     <div className="navWrapper">
       <span className="headerText">Hogwarts</span>
